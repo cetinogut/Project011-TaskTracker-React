c3db377ba24175aebf6e57e51e2a217ba8011f2a
Cetin OGUT <cetinogut.gm@gmail.com>
2021-08-29 18:49:17 +0300

    updated delete  task confirmation

--- src/components/confirmdelete/ConfirmationBox.js
+++ src/components/confirmdelete/ConfirmationBox.js
@@ -1,22 +1,3 @@
-/* define the state */
+//Check a sample in JSX with styled components and
 
-import  { useState } from "react"
-
-const [delTask, setDelTask] = useState(false)
-
-/* if delTask is false, change the display properties of our 
- * two elements and change delTask to true, so that next time 
- * the function is called, the elements are hidden again
- */
-
-export const handleConfirmationBox = () => {
-  if (!delTask) {
-    document.querySelector(".confirm-bg").style.display = "flex"
-    document.querySelector(".container").style.display = "flex"
-    setDelTask(true)
-  } else {
-    document.querySelector(".confirm-bg").style.display = "none"
-    document.querySelector(".container").style.display = "none"
-    setDelTask(false)
-  }
-}
+// https://dev.to/isarisariver/how-to-create-a-custom-confirm-box-with-react-754
